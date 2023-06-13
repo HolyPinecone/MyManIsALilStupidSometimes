6102b10497e4e0a2baee35227e5c0fd5bed31fe8
HolyPinecone <136019832+HolyPinecone@users.noreply.github.com>
2023-06-13 18:36:04 +0300

    They more stupid than i thought: added extra help

--- src/WomanHelper.java
+++ src/WomanHelper.java
@@ -7,16 +7,55 @@ public class WomanHelper {
         Random random = new Random();
 
         while (true) {
-            System.out.println("What is she like today? Choose a mood: angry, happy, sad, quiet, hyper, evil");
-            String mood = scanner.nextLine().toLowerCase();
+            System.out.println("Do you know her mood? (yes/no)");
+            String knowMood = scanner.nextLine().toLowerCase();
 
+            String mood = "";
+            if (knowMood.equals("yes")) {
+                System.out.println("What is her mood?");
+                mood = scanner.nextLine().toLowerCase();
+            } else if (knowMood.equals("no")) {
+                System.out.println("Is she quiet? (yes/no)");
+                String isQuiet = scanner.nextLine().toLowerCase();
+
+                if (isQuiet.equals("yes")) {
+                    System.out.println("Is she mute? (yes/no)");
+                    String isMute = scanner.nextLine().toLowerCase();
+
+                    if (isMute.equals("yes")) {
+                        System.out.println("Her mood is angry:");
+                        mood = "angry";
+                    } else if (isMute.equals("no")) {
+                        System.out.println("Her mood is sad:");
+                        mood = "sad";
+                    }
+                } else if (isQuiet.equals("no")) {
+                    System.out.println("Is she insulting? (yes/no)");
+                    String isInsulting = scanner.nextLine().toLowerCase();
+
+                    if (isInsulting.equals("yes")) {
+                        System.out.println("She is just being evil right now:");
+                        mood = "evil";
+                    } else if (isInsulting.equals("no")) {
+                        System.out.println("Is she moving a lot? (yes/no)");
+                        String isMoving = scanner.nextLine().toLowerCase();
+
+                        if (isMoving.equals("yes")) {
+                            System.out.println("She is hyper:");
+                            mood = "hyper";
+                        } else if (isMoving.equals("no")) {
+                            System.out.println("Her mood is happy!:");
+                            mood = "happy";
+                        }
+                    }
+                }
+            }
             String response = getResponse(mood, random);
             System.out.println(response);
 
             System.out.println("Do you want to analyze another mood? (yes/no)");
-            String continueOption = scanner.nextLine().toLowerCase();
-
-            if (continueOption.equals("no")) {
+            String analyzeAnother = scanner.nextLine().toLowerCase();
+            if (!analyzeAnother.equals("yes")) {
                 break;
             }
         }
